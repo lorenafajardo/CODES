b46ba50895c39f5aba1cd08d9622de77a827aefe
lorenafajardo <87463011+lorenafajardo@users.noreply.github.com>
2021-09-22 23:13:27 -0500

    Helpers

---
+++ Function_arrows.cs
@@ -0,0 +1,9 @@
+namespace ProyPeliculas_G63.Client.Helpers
+{
+    public class Function_arrows
+    {
+    public static string function_convert_mayusc(string value) => value.ToUpper();
+    public static string function_convert_minusc(string value) => value.ToLower();
+
+    }
+}
